1a02056f75af8f01db0065cb64232fd25f23faf8
Adrian González Pardo <gozapaadr@gmail.com>
2020-12-15 19:12:27 -0600

    SQLite controller correction

--- sqliteExample/sqlite_controller.rb
+++ sqliteExample/sqlite_controller.rb
@@ -17,12 +17,12 @@ class SQLITE_CONTROLLER
     @db.execute "CREATE TABLE IF NOT EXISTS #{name_table} (#{params})"
   end
 
-  def insert_data(name_table="ex_table",sing="?",params=["0"],con)
+  def insert_data(name_table="ex_table",sing="?",params=["0"])
     """ Insert data in table """
     @db.execute "INSERT INTO #{name_table} VALUES(#{sing})",params
   end
 
-  def get_all_data(name_table="ex_table",con)
+  def get_all_data(name_table="ex_table")
     """ Query all the data in table """
     @stm=@db.prepare "SELECT*FROM #{name_table}"
     val=@stm.execute
@@ -35,7 +35,7 @@ class SQLITE_CONTROLLER
   end
 
   def get_some_data(name_table="ex_table",from_what="id",
-      logical_where="id<?",where_what=["1"],con)
+      logical_where="id<?",where_what=["1"])
     """ Query some attr in the table  """
     @stm=@bd.prepare "SELECT #{from_what} FROM #{name_table} where #{logical_where}"
     where_what.each_with_index{|i,j|
@@ -44,17 +44,17 @@ class SQLITE_CONTROLLER
     val=@stm.execute
     str=""
     val.each{|i|str+="#{i.join(",")}\n"}
-    stm.close
+    @stm.close
     str
   end
 
   def update_data(name_table="ex_table",set_data="id=?",where_data="id=?",
-      data=[0,2],con)
+      data=[0,2])
     """ Update data """
     @db.execute "UPDATE #{name_table} SET #{set_data} WHERE #{where_data}",data
   end
 
-  def delete_data(name_table="ex_table",where_data="id=?",data=[1],con)
+  def delete_data(name_table="ex_table",where_data="id=?",data=[1])
     """ Delete data in table  """
     @db.execute "DELETE FROM #{name_table} WHERE #{where_data}",data
   end
